a530c46bc9594745e12bf2c3e54913b734b90cd2
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-05 10:42:29 +0300

    feature: strengthen the security

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 import { router as userRouter } from '@src/routes/userRouter';
 
 export const app = express();
 
 // middlewares
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'TOo many request from this IP. PLease try again in an hour!',
+});
+
+app.use('/api', limiter);
+app.use(express.json({ limit: '10kb' }));
+app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
