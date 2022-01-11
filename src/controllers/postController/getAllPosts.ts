80152d242585723b0861f8ddbcdd56f1e96a8c47
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-11 17:30:34 +0300

    feat: added getAllPosts endpoint.

---
+++ src/controllers/postController/getAllPosts.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
+
+import { AppError } from '@src/utils/appError';
+import { tryCatch } from '@src/utils/tryCatch';
+import { Posts } from '@src/models/entities/Post';
+
+export const getAllPosts = tryCatch(async (request: Request, response: Response, next: NextFunction) => {
+  const allPosts = await Posts.find({
+    relations: ['user'],
+  });
+
+  if (allPosts.length === 0) {
+    return next(new AppError('No users exist', 400));
+  }
+
+  const allPostsData = allPosts.map(posts => {
+    const { post, postCreationTime, id, title, user } = posts;
+    const postId = id;
+    const userId = user.id;
+
+    return {
+      title,
+      post,
+      postCreationTime,
+      userId,
+      postId,
+    };
+  });
+
+  return response.status(200).json({
+    status: 'Success',
+    posts: allPosts.length,
+    data: allPostsData,
+  });
+});
--- src/routes/postsRouter.ts
+++ src/routes/postsRouter.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+import { getAllPosts } from '@src/controllers/postController/getAllPosts';
 import { updatePost } from '@src/controllers/postController/updatePost';
 import { validatePostAndTitle } from '@src/routes/validatePostsRoutes';
 import { isAuth } from '@src/middleware/isAuth';
@@ -11,3 +12,4 @@ export const router = express.Router();
 router.route('/createPost').post(isAuth, validatePostAndTitle, createPost);
 router.route('/updatePost').patch(isAuth, validatePostAndTitle, updatePost);
 router.route('/deletePost').delete(isAuth, deletePost);
+router.route('/getAllPosts').get(isAuth, getAllPosts);
