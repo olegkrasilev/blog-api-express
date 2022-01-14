26b39f6e06aad14651feaa8fbc6e858c7fde65f6
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-14 16:04:31 +0300

    fix:(resetPassword): refactored logic

--- src/controllers/userController/resetPassword.ts
+++ src/controllers/userController/resetPassword.ts
@@ -1,21 +1,17 @@
 import crypto from 'crypto';
 
 import { NextFunction, Request, Response } from 'express';
-import { getManager } from 'typeorm';
 import bcrypt from 'bcrypt';
-import { validationResult } from 'express-validator';
 
-import { createRefreshAccessToken } from '@src/utils/createTokenJWT';
 import { tryCatch } from '@src/utils/tryCatch';
 import { AppError } from '@src/utils/appError';
 import { User } from '@src/models/entities/User';
+import { validateRequest } from '@src/utils/express-validator';
 
 export const resetPassword = tryCatch(async (request: Request, response: Response, next: NextFunction) => {
   // 1) Get user based on the token
   const resetToken = request.params.token;
-
   const hashedToken = crypto.createHash('sha256').update(resetToken).digest('hex');
-
   const user = await User.findOne({ passwordResetToken: hashedToken });
 
   if (!user) {
@@ -24,41 +20,33 @@ export const resetPassword = tryCatch(async (request: Request, response: Respons
 
   // 2) If token has not expired, and there is user, set the new resetPassword
   const now = new Date();
-  const entityManager = getManager();
   const newPassword = request.body.password;
 
-  // Validate request for errors with Express-validator
-  const errors = validationResult(request);
+  validateRequest(request, response);
 
-  if (!errors.isEmpty()) {
-    return response.status(400).json({
-      status: 'fail',
-      errors: errors.array(),
-    });
-  }
-
-  const { id, passwordResetExpires } = user;
+  const { passwordResetExpires } = user;
   const newEncryptedPassword = await bcrypt.hash(newPassword, 12);
 
   // 3) Update changedPassword property for the current user
   if (passwordResetExpires > now) {
-    user.encryptedPassword = newEncryptedPassword;
-    // Subtract 1 second to make this condition work : isUserChangedPassword = JWTTimeStamp < changedTimeStamp;
-    user.passwordChangedAt = new Date(now.getTime() - 1000);
-    user.passwordResetExpires = now;
-    user.passwordResetToken = '';
-    await entityManager.save(user);
+    await User.merge(user, {
+      encryptedPassword: newEncryptedPassword,
+      passwordChangedAt: now,
+      passwordResetExpires: now,
+      passwordResetToken: '',
+    }).save();
   }
 
   if (now > passwordResetExpires) {
     return next(new AppError('Password reset time expired. Please try again', 400));
   }
 
-  // 4) Log the user in, send the JWT
-  const token = createRefreshAccessToken(id, response);
+  // 4) Clear jwt and login with new password
 
-  return response.status(200).json({
-    status: 'Success',
-    token,
+  response.clearCookie('jwtAccessToken');
+  response.clearCookie('jwtRefreshToken');
+
+  return response.status(204).json({
+    status: 'success',
   });
 });
