b45c84d14e7d8364b812a1d4afffe1ffc625cf00
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-14 16:24:14 +0300

    fix(updateUser): refactored logic.

--- src/controllers/userController/updateUser.ts
+++ src/controllers/userController/updateUser.ts
@@ -1,45 +1,37 @@
 import { NextFunction, Response } from 'express';
-import { getManager } from 'typeorm';
-import { validationResult } from 'express-validator';
 
 import { AppError } from '@src/utils/appError';
 import { RequestUser } from '@src/types/index';
 import { tryCatch } from '@src/utils/tryCatch';
 import { User } from '@src/models/entities/User';
+import { validateRequest } from '@src/utils/express-validator';
 
 export const updateUser = tryCatch(async (request: RequestUser, response: Response, next: NextFunction) => {
-  const entityManager = getManager();
-  const { id, email, lastName, firstName } = request.body;
+  const { userID, email, lastName, firstName } = request.body;
 
-  if (!(id && email && lastName && firstName)) {
+  if (!(userID && email && lastName && firstName)) {
     return next(new AppError('Please provide id,email, firstName and lastName', 400));
   }
 
-  // Validate request for errors with Express-validator
-  const errors = validationResult(request);
-
-  if (!errors.isEmpty()) {
-    return response.status(400).json({
-      status: 'fail',
-      errors: errors.array(),
-    });
-  }
+  validateRequest(request, response);
 
   const newEmail = email;
 
   // 2) Update user document
-  const [user] = await User.findByIds([id]);
+  const user = await User.findOne(userID);
 
   if (!user) {
     return next(new AppError('This user does not exist.', 404));
   }
 
-  user.email = newEmail;
-  user.firstName = firstName;
-  user.lastName = lastName;
-  await entityManager.save(user);
+  await User.merge(user, { email: newEmail, lastName, firstName }).save();
 
   return response.status(200).json({
     status: 'success',
+    data: {
+      newEmail,
+      lastName,
+      firstName,
+    },
   });
 });
