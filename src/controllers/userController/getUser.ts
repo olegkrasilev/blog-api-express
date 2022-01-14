903cfa3c59cd3bd595caadc122e8763af78a7a13
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-14 15:50:57 +0300

    fix(getUser) : refactored logic.

--- src/controllers/userController/getUser.ts
+++ src/controllers/userController/getUser.ts
@@ -1,15 +1,14 @@
 import { NextFunction, Response } from 'express';
 
 import { RequestUser } from '@src/types/index';
-
 import { AppError } from '@src/utils/appError';
 import { tryCatch } from '@src/utils/tryCatch';
 import { User } from '@src/models/entities/User';
 
 export const getUser = tryCatch(async (request: RequestUser, response: Response, next: NextFunction) => {
-  const { id } = request.params;
+  const { userID } = request.body;
 
-  const user = await User.findOne(id);
+  const user = await User.findOne(userID);
 
   if (!user) {
     return next(new AppError('Current User dont exists', 404));
@@ -19,6 +18,6 @@ export const getUser = tryCatch(async (request: RequestUser, response: Response,
 
   return response.status(200).json({
     status: 'Success',
-    data: { id, email, firstName, lastName },
+    data: { userID, email, firstName, lastName },
   });
 });
