9377f6e29ab76bcf1b905fb2b20f5d7703462490
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-14 13:24:26 +0300

    feature(findComment): documentation.

--- src/utils/findComment.ts
+++ src/utils/findComment.ts
@@ -1,5 +1,11 @@
 import { Comments } from '@src/models/entities/Comment';
 
+/**
+ * @description Find comment in database by userID,postID and commentID
+ * @param  {number} userID
+ * @param  {number} postID
+ * @param  {number} commentID
+ */
 export const findComment = async (userID: number, postID: number, commentID: number) =>
   Comments.findOne(commentID, {
     relations: ['user', 'post'],
