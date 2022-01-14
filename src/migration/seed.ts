3151dbf2a4e1869c44b7e1638541a993687eb541
olegkrasilev <65223358+olegkrasilev@users.noreply.github.com>
2022-01-14 16:25:14 +0300

    fix(seed): removed Up.

--- src/migration/seed.ts
+++ src/migration/seed.ts
@@ -1,13 +1,7 @@
-import { getRepository, MigrationInterface, QueryRunner } from 'typeorm';
-
-import { UserRoleSeed } from '../database/seed/user.seed';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Seeds1642039438616 implements MigrationInterface {
-  public async up(_: QueryRunner): Promise<any> {
-    const userRoleSeed = UserRoleSeed;
-
-    await getRepository('User').save(userRoleSeed);
-  }
+  public async up(_: QueryRunner): Promise<any> {}
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
 }
