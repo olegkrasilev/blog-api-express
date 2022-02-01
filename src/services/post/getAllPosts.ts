c7c6f181edd0d16aa2a8eca5febd88339603d095
olegkrasilev <olegkrasilev@gmail.com>
2022-02-01 10:44:46 +0300

    fix(getAllPosts): response data.

--- src/services/post/getAllPosts.ts
+++ src/services/post/getAllPosts.ts
@@ -6,11 +6,19 @@ import { Posts } from '@src/models/entities/Post';
 export const getAllPosts = tryCatch(async (request: Request, response: Response, next: NextFunction) => {
   const allPosts = await Posts.find({
     select: ['post', 'postCreationTime', 'title', 'id'],
+    relations: ['user'],
+  });
+
+  const AllUsersPosts = allPosts.map(data => {
+    const { title, post, postCreationTime, user } = data;
+    const { firstName, lastName } = user;
+
+    return { title, post, postCreationTime, firstName, lastName };
   });
 
   return response.status(200).json({
     status: 'Success',
-    posts: allPosts.length,
-    data: allPosts,
+    NumberOfPosts: allPosts.length,
+    Posts: AllUsersPosts,
   });
 });
